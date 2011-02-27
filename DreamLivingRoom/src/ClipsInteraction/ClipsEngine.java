e0ea27ac5257c1f140b75172d4ca5b2e03957693
timothee.ledure <timothee.ledure@7cdb87d2-e682-5b22-283f-2b88d5dd7bff>
2011-02-27 15:45:02 +0000

    Our app can now read clips file and ask questions and suggest a list of answers according to the clips file.

--- DreamLivingRoom/src/ClipsInteraction/ClipsEngine.java
+++ DreamLivingRoom/src/ClipsInteraction/ClipsEngine.java
@@ -6,6 +6,8 @@
 package ClipsInteraction;
 
 import CLIPSJNI.*;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  *
@@ -18,30 +20,38 @@ public class ClipsEngine {
     public ClipsEngine(String fileName)
     {
         clipsEnvironment = new Environment();
-        //clipsEnvironment.load(fileName);
+        clipsEnvironment.load(fileName);
         clipsEnvironment.reset();
     }
 
-    public String Run()
+    public void runEnvironment()
     {
-        String clipsResult = "";
-        
         clipsEnvironment.run();
+    }
 
-        String factsRequest = "(find-all-facts ((?f action)) TRUE)";
-        MultifieldValue multifieldValue =
-                (MultifieldValue) clipsEnvironment.eval(factsRequest);
+    public Question getQuestion()
+    {
+        Question clipsResult = new Question();
+        
+        String factsRequest = "(find-all-facts ((?f question)) TRUE)";
+        MultifieldValue multifieldValue = (MultifieldValue) clipsEnvironment.eval(factsRequest);
+
         int listSize = multifieldValue.listValue().size();
         FactAddressValue factAddressValue;
         for (int i=0; i<listSize; i++)
         {
              factAddressValue = (FactAddressValue) multifieldValue.listValue().get(i);
-             clipsResult += factAddressValue.getFactSlot("InsertHereAName").toString();
-             clipsResult += "\n";
+             clipsResult.setText(factAddressValue.getFactSlot("text").toString());
+             clipsResult.setQuestionId(factAddressValue.
+                     getFactSlot("question-id").toString());
+             List<PrimitiveValue> listValue = ((MultifieldValue) factAddressValue.getFactSlot("valid-answers")).listValue();
+             
+             for (PrimitiveValue pValue : listValue)
+             {
+                clipsResult.addAValidAnswer(pValue.getValue().toString());
+             }
         }
 
-        clipsEnvironment.reset();
-         
         return clipsResult;
     }
 }
